15070c6e7e41a3333db8beb6ae3a120f749e940e
Sarah German <sarah@clockwork.com>
2021-12-04 10:07:18 -0600

    Template for future exercises

---
+++ days/template.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+
+
+def part_one(data):
+    return ""
+
+
+def part_two(data):
+    return ""
+
+
+def main():
+    data = open(sys.argv[1]).read().splitlines()
+    print(part_one(data))
+    print(part_two(data))
+
+
+if __name__ == '__main__':
+    main()
